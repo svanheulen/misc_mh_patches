3f286f6d8c2d8d7a898b76e564f81471498a35eb
Seth VanHeulen <svanheulen@gmail.com>
2017-06-09 19:04:48 -0400

    added MHXX custom DLC server patch

---
+++ mhxx/custom_dlc/custom_dlc_patch.s
@@ -0,0 +1,32 @@
+.3ds
+
+.open "code.bin", "patched_code.bin", 0x100000
+
+.include settings_file
+
+.org url_patch_start
+    // copy URL
+    mov r2, #0x80
+    adr r1, url
+    add r0, r4, #0x610
+    add r0, r0, #0xc
+    bl memcpy
+    // enable overriding the default URL
+    mov r0, #1
+    strb r0, [r4,#0x8a]
+    // mark authentication as complete
+    strb r0, [r4,#0x81]
+    b url_patch_continue
+.pool
+url:
+.area 0x80
+    .ascii custom_url,0
+.endarea
+
+.org verify_patch_start
+    // set response data size
+    str r0, [r5,#0x7d4]
+    b verify_patch_continue
+
+.close
+
---
+++ mhxx/custom_dlc/settings/jpn_110.s
@@ -0,0 +1,6 @@
+memcpy equ 0x2e6f1c
+url_patch_start equ 0x22f114
+url_patch_continue equ 0x22f0fc
+verify_patch_start equ 0x230008
+verify_patch_continue equ 0x23035c
+
