1bd571c483c8b212f6d38d217db6be5f5a187b2b
Seth VanHeulen <svanheulen@gmail.com>
2017-08-02 11:30:53 -0400

    updated custom dlc patch for MHStories v1.2.0

--- custom_dlc/mhstories/settings/jpn_110.s
+++
@@ -1,7 +0,0 @@
-memcpy equ 0x29e668
-url_patch_start equ 0x1275f4
-url_patch_continue equ 0x127ad2
-url equ 0x1276a4
-verify_patch_start equ 0x127daa
-verify_patch_continue equ 0x127f1e
-
---
+++ custom_dlc/mhstories/settings/jpn_120.s
@@ -0,0 +1,7 @@
+memcpy equ 0x29f9ec
+url_patch_start equ 0x127684
+url_patch_continue equ 0x127b62
+url equ 0x127734
+verify_patch_start equ 0x127e3a
+verify_patch_continue equ 0x127fae
+
